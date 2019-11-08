8ccb42512d90ad49763e30fe86d6881684f44593
Brian Parsons <brian.parsons.work@gmail.com>
2019-11-07 21:52:02 -0600

    bare bones react setup again

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Cart from './components/cart/cart'
 
 const App = () => {
     return (
         <div className="App">
-            hey!
+            <Cart />
         </div>
     )
 };
---
+++ src/components/cart/cart.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Cart from './cart';
+
+describe('<Cart/>', () => {
+    it('is a test', () => {
+        expect(2).toBe(2);
+    })
+})
